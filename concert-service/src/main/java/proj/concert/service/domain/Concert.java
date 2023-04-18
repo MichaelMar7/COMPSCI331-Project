37b0907e4eb8c4b04717886d896a278c1122d454
ghxstling <dcho282@aucklanduni.ac.nz>
2023-04-19 02:34:23 +1200

    Begin implementation of Concert.java

--- concert-service/src/main/java/proj/concert/service/domain/Concert.java
+++ concert-service/src/main/java/proj/concert/service/domain/Concert.java
@@ -1,20 +1,58 @@
 package proj.concert.service.domain;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import proj.concert.common.jackson.LocalDateTimeDeserializer;
+import proj.concert.common.jackson.LocalDateTimeSerializer;
 
+@Entity
+@Table(name = "CONCERTS")
 public class Concert{
 
     // TODO Implement this class.
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", nullable = false)
+    private Long id;
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+    private String imageName;
+    private String blrb;
+    private Set<LocalDateTime> dates;
+    private List<Performer> performers;
 
-    public Set<LocalDateTime> getDates() {
-        return null;
+    public Concert() {}
+    public Concert(Long id,
+                   String title,
+                   String imageName,
+                   String blrb) {
+        this.id = id;
+        this.title = title;
+        this.imageName = imageName;
+        this.blrb = blrb;
+        dates = new HashSet<>();
+        performers = new ArrayList<>();
     }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return this.title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getImageName() { return imageName; }
+    public void setImageName(String imageName) { this.imageName = imageName; }
+    public String getBlrb() { return blrb; }
+    public void setBlrb(String blrb) { this.blrb = blrb; }
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    public Set<LocalDateTime> getDates() {
+        return dates;
+    }
+    public List<Performer> getPerformers() { return performers; }
 }
