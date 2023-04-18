a53e7c2d0310cdc6a4c7613ecece936dde510a87
ghxstling <dcho282@aucklanduni.ac.nz>
2023-04-19 02:34:38 +1200

    Implemented ConcertMapper.java

---
+++ concert-service/src/main/java/proj/concert/service/mapper/ConcertMapper.java
@@ -0,0 +1,26 @@
+package proj.concert.service.mapper;
+import proj.concert.service.domain.Concert;
+
+public class ConcertMapper {
+
+    static Concert toDomainModel(proj.concert.common.dto.ConcertDTO dtoConcert) {
+        Concert domainConcert = new Concert(
+                dtoConcert.getId(),
+                dtoConcert.getTitle(),
+                dtoConcert.getImageName(),
+                dtoConcert.getBlurb()
+        );
+        return domainConcert;
+    }
+
+    static proj.concert.common.dto.ConcertDTO toDto(Concert concert) {
+        proj.concert.common.dto.ConcertDTO dtoConcert = new proj.concert.common.dto.ConcertDTO(
+                concert.getId(),
+                concert.getTitle(),
+                concert.getImageName(),
+                concert.getBlrb()
+        );
+        return dtoConcert;
+    }
+
+}
