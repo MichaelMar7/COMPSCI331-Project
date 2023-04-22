72a7ab8f97379091ca1d16be38f937fd7f1403dc
MichaelMar7 <michaelmar719@gmail.com>
2023-04-22 16:49:37 +1200

    added GET all, POST, PUT, DELETE methods (not sure if they're done)

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -1,18 +1,23 @@
 package proj.concert.service.services;
 
+import proj.concert.common.dto.ConcertDTO;
 import proj.concert.service.domain.*;
 import proj.concert.service.mapper.ConcertMapper;
 
+import javax.persistence.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import java.util.*;
+import java.net.URI;
+import java.util.stream.Collectors;
 
 @Path("/concerts")
 public class ConcertResource {
-
     EntityManager em = PersistenceManager.instance().createEntityManager();
 
     /*
@@ -34,7 +39,7 @@ public class ConcertResource {
             if (concert == null) {
                 builder = Response.status(404);
             } else {
-                proj.concert.common.dto.ConcertDTO concertDTO = ConcertMapper.toDto(concert);
+                ConcertDTO concertDTO = ConcertMapper.toDto(concert);
                 builder = Response.ok(concertDTO);
             }
 
@@ -47,11 +52,119 @@ public class ConcertResource {
     /*
     Retrieves all concerts from the web service
      */
-//    public Response getAllConcerts() { return Response.noContent().build(); }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllConcerts() {
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            TypedQuery<Concert> concertQuery = em.createQuery("select c from Concert c",Concert.class);
+            List<Concert> concerts = concertQuery.getResultList();
+            List<proj.concert.common.dto.ConcertDTO> concertDTOs = concerts.stream()
+                    .map(concert -> ConcertMapper.toDto(concert))
+                    .collect(Collectors.toList());
+            tx.commit();
+            return Response.ok(concertDTOs).build();
+        } finally {
+            em.close();
+        }
+    }
 //
 //    /*
 //    Retrieves a summary of all concerts from the web service
 //     */
 //    public Response getAllConcertSummaries() { return Response.noContent().build(); }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createConcert(ConcertDTO concertDTO) {
+        Concert concert = ConcertMapper.toDomainModel(concertDTO);
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(concert);
+            tx.commit();
+        } finally {
+            em.close();
+        }
+
+        return Response.created(URI.create("/concerts/" + concert.getId())).entity(ConcertMapper.toDto(concert)).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateConcert(@PathParam("id") long id, ConcertDTO concertDTO) {
+
+        EntityTransaction tx = em.getTransaction();
+        ResponseBuilder builder;
+        Concert updatedConcert = null;
+        try {
+            tx.begin();
+            Concert concert = em.find(Concert.class, id);
+            if (concert == null) {
+                builder = Response.status(Response.Status.NOT_FOUND);
+            }
+            updatedConcert = ConcertMapper.updateFromDto(concertDTO, concert);
+            em.merge(updatedConcert);
+            tx.commit();
+        } finally {
+            em.close();
+        }
+
+        if (updatedConcert == null) {
+            builder = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
+        } else {
+            ConcertDTO updatedConcertDTO = ConcertMapper.toDto(updatedConcert);
+            builder = Response.ok(updatedConcertDTO);
+        }
+
+        return builder.build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteConcert(@PathParam("id") long id) {
+        EntityTransaction tx = em.getTransaction();
+        ResponseBuilder builder;
+        try {
+            tx.begin();
+            Concert concert = em.find(Concert.class, id);
+            if (concert == null) {
+                builder = Response.status(Response.Status.NOT_FOUND);
+            } else {
+                em.remove(concert);
+                builder = Response.noContent();
+            }
+            tx.commit();
+
+        } finally {
+            em.close();
+        }
+
+        return builder.build();
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteAllConcerts() {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Query query = em.createQuery("DELETE FROM Concert");
+            int numDeleted = query.executeUpdate();
+            tx.commit();
+        } finally {
+            em.close();
+        }
+
+        return Response
+                .noContent()
+                .build();
+    }
 
 }
