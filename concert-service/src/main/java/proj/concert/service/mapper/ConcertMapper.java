8fdad37adc4bef2d28112f558dba7e74f91d9722
ghxstling <dcho282@aucklanduni.ac.nz>
2023-04-24 11:11:57 +1200

    Further implementation of toDto method

--- concert-service/src/main/java/proj/concert/service/mapper/ConcertMapper.java
+++ concert-service/src/main/java/proj/concert/service/mapper/ConcertMapper.java
@@ -1,9 +1,18 @@
 package proj.concert.service.mapper;
+
 import proj.concert.service.domain.Concert;
+import proj.concert.common.dto.ConcertDTO;
+import proj.concert.service.domain.Performer;
+import proj.concert.common.dto.PerformerDTO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConcertMapper {
 
-    public static Concert toDomainModel(proj.concert.common.dto.ConcertDTO dtoConcert) {
+    public static Concert toDomainModel(ConcertDTO dtoConcert) {
         Concert domainConcert = new Concert(
                 dtoConcert.getId(),
                 dtoConcert.getTitle(),
@@ -13,13 +22,23 @@ public class ConcertMapper {
         return domainConcert;
     }
 
-    public static proj.concert.common.dto.ConcertDTO toDto(Concert concert) {
-        proj.concert.common.dto.ConcertDTO dtoConcert = new proj.concert.common.dto.ConcertDTO(
+    public static ConcertDTO toDto(Concert concert) {
+        ConcertDTO dtoConcert = new ConcertDTO(
                 concert.getId(),
                 concert.getTitle(),
                 concert.getImageName(),
                 concert.getBlrb()
         );
+        List<PerformerDTO> performers = new ArrayList<>();
+        for (Performer p: concert.getPerformers()) {
+            performers.add(PerformerMapper.toDto(p));
+        }
+        dtoConcert.setPerformers(performers);
+        List<LocalDateTime> dates = new ArrayList<>();
+        for (LocalDateTime d: concert.getDates()) {
+            dates.add(d);
+        }
+        dtoConcert.setDates(dates);
         return dtoConcert;
     }
 
