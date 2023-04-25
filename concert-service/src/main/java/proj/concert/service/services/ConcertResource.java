c21e757cf60b4f2fcbbf5b07763842cface91bbd
ghxstling <dcho282@aucklanduni.ac.nz>
2023-04-25 20:29:28 +1200

    Fixed HTTP 404 NotFoundError that's breaking our test cases

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
-@Path("/concerts")
+@Path("/concert-service/concerts")
 public class ConcertResource {
 
     EntityManager em = PersistenceManager.instance().createEntityManager();
