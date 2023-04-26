5ca14d97b0ca325abbfd1b4c78576c77aeaf0941
Clavides <111670014+Clavides@users.noreply.github.com>
2023-04-26 12:13:48 +1200

    implemented Booking.java

--- concert-service/src/main/java/proj/concert/service/domain/Booking.java
+++ concert-service/src/main/java/proj/concert/service/domain/Booking.java
@@ -1,4 +1,56 @@
 package proj.concert.service.domain;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "BOOKING")
 public class Booking {
+    @Id
+    @GeneratedValue
+    private long bookingId;
+    private long concertId;
+    private LocalDateTime date;
+    @OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.ALL, CascadeType.REMOVE})
+    private Set<Seat> seats = new HashSet<>();
+
+    public Booking() {
+    }
+
+    public Booking(long concertId, LocalDateTime date, Set<Seat> seats) {
+        this.concertId = concertId;
+        this.date = date;
+        this.seats = seats;
+    }
+
+    public long getConcertId() {
+        return concertId;
+    }
+
+    public void setConcertId(long concertId) {
+        this.concertId = concertId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Set<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Set<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public long getBookingId() {
+        return this.bookingId;
+    }
+
 }
